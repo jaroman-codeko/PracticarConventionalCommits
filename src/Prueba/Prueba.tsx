8520867add78c945eaa14311d39c700062253450
jose antonio <joseCodeko@gmail.com>
2025-03-18 08:55:11 +0100

    feat:added a pokemon while the button reset

--- src/Prueba/Prueba.tsx
+++ src/Prueba/Prueba.tsx
@@ -1,42 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { api } from "../services/api.js";
 
-interface Ejemplo{
-    title:string,
-    disabled:boolean
+interface Ejemplo {
+  title: string;
+  disabled: boolean;
 }
-
-
 
 const Btn = () => {
+  const [click, setClick] = useState(false);
+  const [datos, setDatos] = useState("Pulsame");
+  const [img, setImg] = useState("");
 
-    const  [click, setClick] = useState(false);
-    const  [datos, setDatos] = useState("Pulsame");
+
+  useEffect(() => {
+  }, [click])
+  
 
 
-    const handleClick= ()=>{
-        click ? console.log("Click desactivado"):setClick(!click)
+  const handleSacarPokemon = async () => {
+    const poke = await sacarPokemon();
+    return poke;
+  };
 
-        setDatos("Has pulsado el boton");
-        setTimeout(()=>{
-            setClick(!!click)
-            setDatos("Pulsame");
+  const creaNumeroAleatorio = () => Math.floor(Math.random() * 900);
 
-        },1000)
+  const sacarPokemon = async () => {
+    const {
+      data: {
+        name,
+        sprites: {
+          other: {
+            showdown: { front_default },
+          },
+        },
+      },
+    } = await api.get(`/pokemon/${creaNumeroAleatorio()}`);
 
-    }
+    return {
+      name,
+      front_default,
+    };
+  };
 
+
+
+  const handleClick = () => {
+    click ? console.log("Click desactivado") : setClick(!click);
+
+
+    setDatos("Loading...");
     
-    return(
-        <button disabled={click}  onClick={handleClick} >{datos}</button>
-    )
+    handleSacarPokemon().then( el=>{
+        setImg(el.front_default)
+        setDatos("")
+    }).catch(()=>console.log("error"))
 
-}
+    setTimeout(() => {
+      setClick(!!click);
+      setDatos("Pulsame");
+      setImg("")
+    }, 3000);
+  };
 
-
-
+  return (
+    <button disabled={click} onClick={handleClick}>
+        {
+            img && <img src={img} alt="imagen de un pokemon " width={"75px"} />
+        }
+      {datos}
+    </button>
+  );
+};
 
 export const Prueba = () => {
-  return (
-    <Btn title="Datos de ejemplo" disabled={false}  />
-        )
-}
+  return <Btn  />;
+};
